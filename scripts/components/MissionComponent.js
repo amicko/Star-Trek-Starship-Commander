2160d90b4d7b5413a58855ff45ccbfb95d6e23ea
Aaron Micko <gmicko@hotmail.com>
2015-11-04 08:56:50 -0600

    completed mission attempt function and create character

--- scripts/components/MissionComponent.js
+++ scripts/components/MissionComponent.js
@@ -3,6 +3,7 @@ var React = require('react');
 
 //components
 var MissionStatsComponent = require('./MissionStatsComponent.js');
+var DilithiumCounterComponent = require('./DilithiumCounterComponent.js')
 
 module.exports = React.createClass({
 	getInitialState: function() {
@@ -10,10 +11,23 @@ module.exports = React.createClass({
 			mainBox: 'missions',
 			missionDrop: null,
 			sectorMissions: [],
-			sector: []
+			sector: [],
+			character: [],
+			// charDilithium: []
 		};
 	},
 	componentWillMount: function() {
+
+		var CharacterModel = Parse.Object.extend('CharacterModel');
+		var CharacterQuery = new Parse.Query(CharacterModel);
+
+		CharacterQuery.equalTo('objectId', this.props.characterId)
+		.find().then((character) => {
+			// console.log(character[0].get('GoldPressedLatinum'))
+			this.setState({
+				character: character
+			})
+		})
 		
 
 		var sectorId = this.props.sectorId;
@@ -35,6 +49,32 @@ module.exports = React.createClass({
 		})
 	},
 	render: function() {
+		var that = this;
+		var charName = this.state.character.map((character) => {
+			return character.get('Name')
+		})
+		var charDil = this.state.character.map((character) => {
+			return character.get('Dilithium')
+		})
+		var charGPL = this.state.character.map((character) => {
+			return character.get('GoldPressedLatinum')
+		})
+
+		// setInterval(function() {
+		// 	// console.log(that.state.character[0])
+		// 	that.state.character[0].set('Dilithium', (parseFloat(charDil) + 1))
+		// 	that.state.character[0].save(null, {
+		// 		success: function(CharacterModel) {
+		// 			// console.log('Dilithium Increased')
+		// 			that.setState({
+		// 				charDilithium: charDil
+		// 			})
+		// 		}
+		// 	})
+		// }, 3000)
+
+		// console.log(charDil[0])
+
 		var sectorMissions = this.state.sectorMissions.map((missions, index) => {
 			return (
 					<div key={'mission'+index} className="sectorMission">
@@ -48,7 +88,7 @@ module.exports = React.createClass({
 		this.sectorMissionStats = this.state.sectorMissions.map((mission, index) => {
 			var style = {display: 'none', 'zIndex': '999', 'marginLeft': '-25em', width: '50%', height: '75.5vh', float: 'right'};
 			return (
-				<MissionStatsComponent toggle={this.onBackground} style={style} key={index} missionName={mission.get('Name')} missionLore={mission.get('Lore')} lvlReq={mission.get('lvlReq')} time={mission.get('TimeToCompletion')} typeReq={mission.get('TypeReq')} neededStat={mission.get('NeededStat')} rewardXP={mission.get('RewardXP')} rewardGPL={mission.get('RewardGPL')} />
+				<MissionStatsComponent toggle={this.onBackground} style={style} key={index} missionName={mission.get('Name')} missionLore={mission.get('Lore')} lvlReq={mission.get('lvlReq')} time={mission.get('TimeToCompletion')} typeReq={mission.get('TypeReq')} neededStat={mission.get('NeededStat')} rewardXP={mission.get('RewardXP')} rewardGPL={mission.get('RewardGPL')} characterId={this.props.characterId}/>
 			)
 		})
 
@@ -83,10 +123,9 @@ module.exports = React.createClass({
 				<a href={'#sector-map/' + this.props.characterId}className="sectorMapIcon"></a>
 				<a href="#" className="userSettingsIcon"></a>
 				<div className="userHUD">
-					<button className="HUDFiller HUDButton" onClick={this.onCharStats}>Character Name</button>
-					<div className="HUDFiller">Dilithium Amount</div>
-					<div className="HUDFiller">Gold-Pressed Latinum Amount</div>
-					<button className="HUDFiller HUDButton" onClick={this.onCharStats}>Starship Name</button>
+					<button className="HUDFiller HUDButton" onClick={this.onCharStats}>{charName}</button>
+					<div className="HUDFiller"><DilithiumCounterComponent characterId={this.props.characterId}/></div>
+					<div className="HUDFiller">GPL: {charGPL}</div>
 				</div>
 				<div className="sectorBlockContainer">
 					{dropDown}
